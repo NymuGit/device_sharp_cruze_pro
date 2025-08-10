07c9fa256ff16983fd13784acc29bdc6d217b55e
Nymu ? Anonym : Ahmedeeya <145840403+NymuGit@users.noreply.github.com>
2025-08-10 12:39:31 +0800

    Update omni_cruze_pro.mk
    Setting tipis2

--- omni_cruze_pro.mk
+++ omni_cruze_pro.mk
@@ -1,7 +1,8 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := cruze_pro
 PRODUCT_NAME := omni_cruze_pro
 PRODUCT_BRAND := Sharp
 PRODUCT_MODEL := SH-C04
 PRODUCT_MANUFACTURER := TCL
+PRODUCT_RELEASE_NAME := cruze_pro
